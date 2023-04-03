60ee88f834434b50ec1f955202c69960b289051b
Audrey Guigo <guigo.audrey@gmail.com>
2023-04-03 14:36:48 +0200

    controller guest

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,5 +1,20 @@
 class GuestsController < ApplicationController
-  def
-    
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.new(params[:guest])
+    @guest.save
+  end
+
+  def edit
+    @guest = Guest.find(params[:id])
+  end
+
+  private
+
+  def guest_params
+    params.require(:guest).permit(:nom, :prenom, :email)
   end
 end
