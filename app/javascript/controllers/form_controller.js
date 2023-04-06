bdb8b5531b2353e73c738e966413bb0ded41bfa5
Theophile Thiberge <tthiberge@gmail.com>
2023-04-06 02:32:03 +0200

    Default behaviour if informing of ABSENCE

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,18 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
 
-  static targets = ["infos", "presence"]
+  static targets = ["infos", "presence", "plusUn", "adults", "kids", "brunch", "accomodation"]
 
   connect() {
 
     console.log("Starting")
     // console.log(this.element)
     // console.log(this.infosTarget)
+    // console.log(this.plusUnTarget)
+    // console.log(this.adultsTarget)
+    // console.log(this.kidsTarget)
+    // console.log(this.brunchTarget.value)
+    // console.log(this.accomodationTarget)
   }
 
   reveal(event) {
     console.log('revealing')
-    // console.log(this.presenceTarget.value) // Renvoie tout le temps Yes... Pourquoi ça ne renvoie pas No quand je clique sur No?
+    // console.log(this.presenceTarget.value) // Renvoie tout le temps Yes... Pourquoi ça ne renvoie pas No quand je clique sur No? 🤨
     // console.log(event.currentTarget) // Donne Yes or No en value
     // console.log(event.currentTarget.value)
 
@@ -27,10 +32,34 @@ export default class extends Controller {
 
 
     if (event.currentTarget.value === "Yes") {
-      this.infosTarget.classList.remove("d-none")
+      this.infosTarget.classList.remove("dnone") // CHANGE
+      // et réinitialiser à unchecked les différentes choses qui auraient pu être cochées par défaut si on passe du non au oui (nil? vide? undefined?)
     } else if (event.currentTarget.value === "No") {
-      this.infosTarget.classList.add("d-none")
-    }
+      this.infosTarget.classList.add("dnone") // CHANGE
+      // Et ajouter des valeurs pour les élements du dessous
+      // ADULTES: Je parcours les choix du nombre d'adultes et je dis de cliquer celui qui vaut 0
+      this.adultsTargets.forEach((target) => {
+        if (target.value == 0)
+        target.click()
+      })
 
+      // ENFANTS: Je parcours les choix du nombre d'enfants et je dis de cliquer celui qui vaut 0
+      this.kidsTargets.forEach((target) => {
+        if (target.value == 0)
+        target.click()
+      })
+
+      // Brunch:
+      this.brunchTargets.forEach((target) => {
+        if (target.value === "No")
+        target.click()
+      })
+
+      // Hebergement:
+      this.accomodationTargets.forEach((target) => {
+        if (target.value.includes("Pas besoin"))
+        target.click()
+      })
+    }
   }
 }
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -5,10 +5,12 @@ class Guest < ApplicationRecord
   # collection: (plus_un == true) ? GUEST::HEBERGEMENT_MULTI : GUEST::HEBERGEMENT_SOLO
 
   # Presence
-  validates :prenom, :nom, :email, :participation, :plus_un, :nb_adultes, :nb_enfants, :brunch, :hebergement, presence: true
+  validates :prenom, :nom, :email, :participation, :nb_adultes, :nb_enfants, :brunch, :hebergement, presence: true
+  # J'ai enlevé le validates presence pour :plus_un car ça permet de faciliter le stimulus du formulaire
+  # et de toute façon ils peuvent ensuite mettre le nombre de personnes qui seront là ensuite
 
   # Spécificités
   validates :email, uniqueness: true
-  validates :nb_adultes, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 7}
-  validates :nb_enfants, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 7}
+  validates :nb_adultes, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 6}
+  validates :nb_enfants, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 6}
 end
