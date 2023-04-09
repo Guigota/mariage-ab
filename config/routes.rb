1facba5d985eb51b8b483952b8218f375b357fd7
Audrey Guigo <guigo.audrey@gmail.com>
2023-04-09 11:45:15 +0200

    trying the toggle navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#homebis"
 
   resources :guests, only: %i[new create edit update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
