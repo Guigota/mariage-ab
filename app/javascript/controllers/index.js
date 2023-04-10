4b6a67a5b4e57cd0b2bd303e46d028b761b821d0
Theophile Thiberge <tthiberge@gmail.com>
2023-04-10 23:10:38 +0200

    Implementing autogrow

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,11 +10,6 @@ application.register("form", FormController)
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-
-
-
-import { Application } from '@hotwired/stimulus'
-import TextareaAutogrow from 'stimulus-textarea-autogrow'
-
-const application = Application.start()
-application.register('textarea-autogrow', TextareaAutogrow)
+// Mathieu
+import TextareaAutogrow from './textarea_autogrow_controller'
+application.register("textarea-autogrow",  TextareaAutogrow)
